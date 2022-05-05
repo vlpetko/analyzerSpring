54bb43ac2e8b33fb4e7e7aa210706e456966327e
Владимир Петько <petko.v@yandex.ru>
2022-05-05 21:12:03 +0600

    create_packages

---
+++ src/main/java/ru/vlpetko/analyzerspring/AnalyzerSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.vlpetko.analyzerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyzerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vlpetko/analyzerspring/AnalyzerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlpetko.analyzerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
